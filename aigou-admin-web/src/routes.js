66e4d8ce5f109f7c511aabe3e54acdab19c48cb9
solargen <solargen@qq.com>
2019-10-12 18:32:43 +0800

    商品品牌和类型的维护

--- aigou-admin-web/src/routes.js
+++ aigou-admin-web/src/routes.js
@@ -11,6 +11,7 @@ import Page6 from './views/nav3/Page6.vue'
 import echarts from './views/charts/echarts.vue'
 
 import Brand from './views/product/Brand'
+import ProductType from './views/product/ProductType'
 
 let routes = [
     {
@@ -43,7 +44,7 @@ let routes = [
         iconCls: 'el-icon-message',//图标样式class
         children: [
             { path: '/brand', component: Brand, name: '品牌管理'},
-            { path: '/type', component: Table, name: '类型管理' },
+            { path: '/productType', component: ProductType, name: '类型管理' },
             { path: '/specification', component: Form, name: '属性管理' },
             { path: '/product', component: user, name: '商品管理' },
         ]
